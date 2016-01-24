ac0a3ceb517914fd6e7b4e9678e6cbde87e8435f
Andy Carter <andy1704@gmail.com>
2016-01-24 13:32:44 +0000

    New Cake 3.x tests.

---
+++ tests/TestCase/View/Helper/InlineCssHelperTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace InlineCss\TestCase\View\Helper;
+
+use Cake\TestSuite\TestCase;
+use Cake\Network\Request;
+use Cake\View\View;
+use InlineCss\View\Helper;
+
+class InlineCssHelperTest extends TestCase
+{
+
+    public $InlineCss = null;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->View = new View(new Request());
+        $this->InlineCss = new InlineCssHelper($this->View);
+    }
+
+    public function testAfterLayout()
+    {
+        $html = $this->View->Blocks->set('content', '<style type="text/css">a{color:red}</style><p><a href="#">Test</a></p>');
+
+        $this->InlineCss->afterLayout(null);
+
+        $expected = '<p><a href="#" style="color: red;">Test</a></p>';
+        $this->assertContains($expected, $this->View->Blocks->get('content'));
+    }
+
+}
